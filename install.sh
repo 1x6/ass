1b48813731969f87307347b840a1e444c477446f
xeny <44981148+1x6@users.noreply.github.com>
2021-07-25 09:43:14 +0100

    add stuff

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
-sudo apt update
+apt update
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 cat /etc/apt/sources.list.d/nodesource.list
-sudo apt -y install nodejs
+apt -y install nodejs
 npm install -g npm@latest
+git clone https://github.com/1x6/ass.git && cd ass/
+npm i
